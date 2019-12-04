386c15a54a2d4c519c72d9b08adfbbb1be5bd746
good-singer <15732658216@163.com>
2019-12-04 17:23:07 +0800

    '完成注册表单'

---
+++ client/src/views/404.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="notfound">
+    <img src="../assets/404.gif" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  components: {}
+}
+</script>
+
+<style scoped>
+.notfound {
+  width: 100%;
+  height: 100%;
+  display: flex;
+}
+.notfound img {
+  width: 100%;
+  height: 100%;
+}
+</style>
